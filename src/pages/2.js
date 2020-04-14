9192571786f9e95ffa1088d4f4cd5ba7d9dff90f
Joseph Koon <josephandrewkoon@gmail.com>
2020-04-13 23:51:34 -0700

    updates

--- src/pages/2.js
+++ src/pages/2.js
@@ -13,6 +13,8 @@ export default () => (
 		<img height='200' src='https://lh3.googleusercontent.com/proxy/_-mxwGqzAu_WFizV0JAyxZ-r3r-5ekOYAkRdI45-TiiBuhHysGaC6gjgjKEZ9PB_6guimge92KfaGu78uTtx-deATaKX0nAZliYxHZg-vyyO8cG_owryqEP3oao9gblDANrQnido'/>
 		<p>Saturday Night Live.. Over Zoom</p>
 		<img height='200' src='https://i.ytimg.com/vi/fp1K8M627wE/maxresdefault.jpg'/>
+		<p>3D Printed Ventilators</p>
+		<img height='200' src='https://bloximages.chicago2.vip.townnews.com/wyomingnews.com/content/tncms/assets/v3/editorial/e/76/e766f0b2-e0fd-5607-a7b7-763cf00afd4a/5e83f07a9b669.image.jpg?resize=1200%2C900'/>
 		<p>Breathing Mask Fashion</p>
 		<img height='200' src='https://lifestyle.mb.com.ph/wp-content/uploads/covid-fashion-banner.jpg'/>
 		
