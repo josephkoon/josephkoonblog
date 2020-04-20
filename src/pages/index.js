dacd12f726956e1d09f06a8b04438a0ef2447727
Joseph Koon <josephandrewkoon@gmail.com>
2020-04-20 11:13:45 -0700

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 
 
 export default () => (
-	<div style={{paddingLeft:'25%', paddingRight:'25%', textAlign:'center'}}>
+	<div style={{paddingTop:'20px', paddingBottom:'20px', paddingLeft:'25%', paddingRight:'25%', textAlign:'center'}}>
 		
 		<h1>Joseph Koon Blog</h1>
 		<img style={{width:'100%'}} src="https://juddfoundation.org/wp-content/uploads/2019/03/IS.26_4F5A8669-1024x682.jpg" alt="" />
