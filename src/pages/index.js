4561d60d8385baaab1938ae9e6634e5e3c7c0fd1
Joseph Koon <josephandrewkoon@gmail.com>
2020-04-30 21:48:32 -0700

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ export default () => (
 
 		<p>something I'm listening to : </p>
 
-<iframe width="100%" height="200" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/76034666&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+<iframe width="100%" height="150" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/76034666&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
 
 		<br/>
 		<br/>
